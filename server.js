dd39a88aa5f1954dcbc89eaf06f7d2c921a92b47
Michael Wood <michaelwood33311@icloud.com>
2022-07-31 09:56:34 -0400

    refactored code to use callbacks when processing requests to allow internal functions to also make requests

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const REST_URL = "https://pymnts.com";
 const SECRET_KEY = 'ksdndkklw4890dfio409dfjoe0509e5oifdhrehhioers';
 
 // import required packages
+const serializerr = require('serializerr');
 const express = require('express');
 const cors = require('cors');
 const https = require('https');
@@ -76,21 +77,19 @@ const sanitizeString = str => {
     return encodeURIComponent(str);
 }
 
-const handleGet = async (endpoint, baseKey, req, res) => {
-    // generate a unique key
+const processRequest = async (endpoint, baseKey, req, successCallback, res = null, errorCallback = null) => {
+    console.log('processRequest', endpoint);
     let key = baseKey;
+
+    const urlParams = Object.keys(req.params);
+    for (let i = 0; i < urlParams.length; ++i) key += `::${sanitizeString(urlParams[i])}:${sanitizeString(req.params[urlParams[i]])}`;
+
+    const queryParams = Object.keys(req.query);
+    for (let i = 0; i < queryParams.length; ++i) key += `::${sanitizeString(queryParams[i])}:${sanitizeString(req.query[queryParams[i]])}`;
     
-        // add any key/value pairs from the query parameters
-        const queryParams = Object.keys(req.query);
-        for (let i = 0; i < queryParams.length; ++i) key += `::${sanitizeString(queryParams[i])}:${sanitizeString(req.query[queryParams[i]])}`;
-            
-        // add any key/value pairs from the url route params (e.g. /:id)
-        const urlParams = Object.keys(req.params);
-        for (let i = 0; i < urlParams.length; ++i) key += `::${sanitizeString(urlParams[i])}:${sanitizeString(req.params[urlParams[i]])}`;
-            
     // check if key is in redis. If so send the JSON parsed result and we are done.
     const redisVal = await redis.get(key);
-    if (redisVal) return res.status(200).json(JSON.parse(redisVal));
+    if (redisVal) return successCallback(key, JSON.parse(redisVal), res);
 
     // if there are url params then replace the params in the endpoint. E.g. replace :id with the id value itself.
     for (let i = 0; i < urlParams.length; ++i) endpoint = endpoint.replace(`/:${urlParams[i]}`, `/${req.params[urlParams[i]]}`);
@@ -112,23 +111,78 @@ const handleGet = async (endpoint, baseKey, req, res) => {
 
     // make the request
 
+    console.log('request', request);
+    
     //console.log(endpoint, key);
+
     axios(request)
     .then(response => {
-        redis.set(key, JSON.stringify(response.data));
-        res.status(200).json(response.data);
+        const { data } = response;
+        redis.set(key, JSON.stringify(data));
+        return successCallback(key, data, res);
+        
     })
     .catch(err => {
-        console.error(`${req.url}: error!`);
-        res.status(400).json(err.response.data);
+        console.error(serializerr(err));
+        if (errorCallback) errorCallback(key, err, res);
     })
 }
 
-app.get('/wp-json/wp/v2/posts', async (req, res) => handleGet ('/wp-json/wp/v2/posts', 'posts', req, res));
+const successfulGet = (key, data, res) => {
+    return res.status(200).json(data);
+}
 
-app.get('/wp-json/wp/v2/media/:id', async (req, res) => handleGet('/wp-json/wp/v2/media/:id', 'mediaId', req, res));
-   
-app.get('/wp-json/wp/v2/categories/:id', async (req, res) => handleGet('/wp-json/wp/v2/categories/:id', 'categoriesId', req, res));
+const unsuccessfulGet = (key, err, res) => {
+    return res.status(400).send('invalid request');
+}
+
+const handleGet = (endpoint, baseKey, req, res) => {
+    processRequest(endpoint, baseKey, req, successfulGet, res, unsuccessfulGet);
+}
+
+// Generic WP REST API endpoints
+
+app.get('/wp-json/wp/v2/posts/:id',  (req, res) => handleGet('/wp-json/wp/v2/posts/:id', 'postsId', req, res));
+app.get('/wp-json/wp/v2/posts',  (req, res) => handleGet ('/wp-json/wp/v2/posts', 'posts', req, res));
+
+app.get('/wp-json/wp/v2/categories/:id',  (req, res) => handleGet('/wp-json/wp/v2/categories/:id', 'categoriesId', req, res));
+app.get('/wp-json/wp/v2/categories/',  (req, res) => handleGet('/wp-json/wp/v2/categories/', 'categories', req, res));
+
+app.get('/wp-json/wp/v2/tags/:id',  (req, res) => handleGet('/wp-json/wp/v2/tags/:id', 'tagsId', req, res));
+app.get('/wp-json/wp/v2/tags/',  (req, res) => handleGet('/wp-json/wp/v2/tags/', 'tags', req, res));
+
+app.get('/wp-json/wp/v2/pages/:id',  (req, res) => handleGet('/wp-json/wp/v2/pages/:id', 'pagesId', req, res));
+app.get('/wp-json/wp/v2/pages/',  (req, res) => handleGet('/wp-json/wp/v2/pages/', 'pages', req, res));
+
+app.get('/wp-json/wp/v2/comments/:id',  (req, res) => handleGet('/wp-json/wp/v2/comments/:id', 'commentsId', req, res));
+app.get('/wp-json/wp/v2/comments/',  (req, res) => handleGet('/wp-json/wp/v2/comments/', 'comments', req, res));
+
+app.get('/wp-json/wp/v2/taxonomies/:id',  (req, res) => handleGet('/wp-json/wp/v2/taxonomies/:id', 'taxonomiesId', req, res));
+app.get('/wp-json/wp/v2/taxonomies/',  (req, res) => handleGet('/wp-json/wp/v2/taxonomies/', 'taxonomies', req, res));
+
+app.get('/wp-json/wp/v2/media/:id',  (req, res) => handleGet('/wp-json/wp/v2/media/:id', 'mediaId', req, res));
+app.get('/wp-json/wp/v2/media/',  (req, res) => handleGet('/wp-json/wp/v2/media/', 'media', req, res));
+
+app.get('/wp-json/wp/v2/types/:id',  (req, res) => handleGet('/wp-json/wp/v2/types/:id', 'typesId', req, res));
+app.get('/wp-json/wp/v2/types/',  (req, res) => handleGet('/wp-json/wp/v2/types/', 'types', req, res));
+
+app.get('/wp-json/wp/v2/search/',  (req, res) => handleGet('/wp-json/wp/v2/search/', 'search', req, res));
+
+// Custom endpoints
+
+// app.get('/wp-json/wp/v2/custom/page-of-posts/:curPage/:numPerPage', (req, res) => {
+//     let endpoint = '/wp-json/wp/v2/posts';
+//     let baseKey = 'posts';
+//     let request = {
+//         query: {
+//             type: 'posts',
+//             per_page: req.params.numPerPage,
+//             page: req.params.curPage
+//         }
+//     }
+//     result = aw
+// });
+
 
 const httpsServer = https.createServer({
   key: fs.readFileSync(`/etc/letsencrypt/live/${DOMAIN}/privkey.pem`),
